33cecb7627654229b6a1a6a99750abdc6cf911df
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-07-13 07:45:52 +0100

    add a stdin stdout lock in main for redacting multiple line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,56 @@
 use crate::ipv4::{IPv4Redactor, Redact};
+use std::io::{BufRead, BufReader, Write};
 
 mod ipv4;
 
 fn main() {
     let redactor = IPv4Redactor::new();
-    redactor.redact("example");
+    let stdin = std::io::stdin().lock();
+    let mut stdout = std::io::stdout().lock();
+
+    redact_logs(BufReader::new(stdin), &mut stdout, &redactor)
+        .unwrap_or_else(|_| println!("failed to redact"));
+}
+
+fn redact_logs(
+    input: impl BufRead,
+    output: &mut impl Write,
+    redactor: &impl Redact,
+) -> std::io::Result<()> {
+    for line in input.lines() {
+        writeln!(output, "{}", redactor.redact(&line?))?;
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ipv4::IPv4Redactor;
+    use crate::redact_logs;
+
+    #[test]
+    fn single_empty_line() {
+        let input = vec![];
+        let mut output = vec![];
+        assert!(redact_logs(&input[..], &mut output, &IPv4Redactor::new()).is_ok());
+        assert_eq!(input, output);
+    }
+
+    // TODO: handle single line without newline
+
+    #[test]
+    fn single_clean_line() {
+        let input = b"clean\n".to_vec();
+        let mut output = vec![];
+        assert!(redact_logs(&input[..], &mut output, &IPv4Redactor::new()).is_ok());
+        assert_eq!(input, output);
+    }
+
+    #[test]
+    fn multiple_clean_lines() {
+        let input = b"clean\nline\nagain\n".to_vec();
+        let mut output = vec![];
+        assert!(redact_logs(&input[..], &mut output, &IPv4Redactor::new()).is_ok());
+        assert_eq!(input, output);
+    }
 }
