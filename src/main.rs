d2bded4d34bcbbcec93bfd2a0d580fe7eaf8bbcd
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-07-17 14:14:30 +0100

    add clap arg parsing to specify which redactors to use

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
+use clap::Parser;
 use redacta::ipv4::{IPv4Redactor, Redact};
+use std::borrow::Cow;
 use std::io::{BufRead, BufReader, Write};
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Enable IPv4 redaction
+    #[arg(short, long)]
+    ipv4: bool,
+}
+
 fn main() {
-    let redactor = IPv4Redactor::default();
+    let args = Args::parse();
+
+    let mut redactors = Vec::<Box<dyn Redact>>::new();
+
+    if args.ipv4 {
+        redactors.push(Box::new(IPv4Redactor::default()))
+    }
+
     let stdin = std::io::stdin().lock();
     let mut stdout = std::io::stdout().lock();
 
-    redact_logs(BufReader::new(stdin), &mut stdout, &redactor)
+    redact_logs(BufReader::new(stdin), &mut stdout, &redactors)
         .unwrap_or_else(|_| println!("failed to redact"));
 }
 
 fn redact_logs(
     mut input: impl BufRead,
     output: &mut impl Write,
-    redactor: &impl Redact,
+    redactors: &[Box<dyn Redact>],
 ) -> anyhow::Result<()> {
     let mut buffer = Vec::with_capacity(1024);
+    let mut owned_buffer = String::with_capacity(1024);
+
     while input.read_until(b'\n', &mut buffer)? != 0 {
-        write!(output, "{}", redactor.redact(std::str::from_utf8(&buffer)?))?;
+        let mut line = std::str::from_utf8(&buffer)?;
+
+        for redactor in redactors {
+            match redactor.redact(line) {
+                Cow::Owned(redacted) => {
+                    owned_buffer.clear();
+                    owned_buffer.push_str(&redacted);
+                    line = &owned_buffer;
+                }
+                _ => continue,
+            }
+        }
+
+        write!(output, "{}", line)?;
         buffer.clear();
     }
+
     Ok(())
 }
 
@@ -32,7 +65,14 @@ mod tests {
     fn single_empty_line() {
         let input = vec![];
         let mut output = vec![];
-        assert!(redact_logs(&input[..], &mut output, &IPv4Redactor::default()).is_ok());
+        assert!(
+            redact_logs(
+                &input[..],
+                &mut output,
+                &[Box::new(IPv4Redactor::default())]
+            )
+            .is_ok()
+        );
         assert_eq!(input, output);
     }
 
@@ -40,7 +80,14 @@ mod tests {
     fn single_line_without_newline() {
         let input = b"no newline".to_vec();
         let mut output = vec![];
-        assert!(redact_logs(&input[..], &mut output, &IPv4Redactor::default()).is_ok());
+        assert!(
+            redact_logs(
+                &input[..],
+                &mut output,
+                &[Box::new(IPv4Redactor::default())]
+            )
+            .is_ok()
+        );
         assert_eq!(input, output);
     }
 
@@ -48,7 +95,14 @@ mod tests {
     fn single_line_with_newline() {
         let input = b"newline\n".to_vec();
         let mut output = vec![];
-        assert!(redact_logs(&input[..], &mut output, &IPv4Redactor::default()).is_ok());
+        assert!(
+            redact_logs(
+                &input[..],
+                &mut output,
+                &[Box::new(IPv4Redactor::default())]
+            )
+            .is_ok()
+        );
         assert_eq!(input, output);
     }
 
@@ -56,7 +110,14 @@ mod tests {
     fn multiple_clean_lines() {
         let input = b"clean\nline\nagain\n".to_vec();
         let mut output = vec![];
-        assert!(redact_logs(&input[..], &mut output, &IPv4Redactor::default()).is_ok());
+        assert!(
+            redact_logs(
+                &input[..],
+                &mut output,
+                &[Box::new(IPv4Redactor::default())]
+            )
+            .is_ok()
+        );
         assert_eq!(input, output);
     }
 }
